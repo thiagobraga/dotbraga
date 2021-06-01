c7cf148a0dd16b4818dd32bfe2114cbd273d6ead
Thiago Braga <thibraga06@gmail.com>
2021-06-01 09:21:06 -0300

    chore: add todo for grub-reboot script

--- src/dual-boot/windows.sh
+++ src/dual-boot/windows.sh
@@ -2,5 +2,7 @@
 
 # Grub will temporary change the default boot option
 # and restore to the original after reboot.
+
+# TODO: Detect Windows dynamically. It needs to filter submenus from list.
 sudo grub-reboot "Windows 10 (em /dev/sda2)"
 sudo reboot
