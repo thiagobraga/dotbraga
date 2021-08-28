bad462b9b0564dc11b2379daeff3169a097cb7d8
Thiago Braga <thibraga06@gmail.com>
2021-08-28 11:42:27 -0300

    feat: Small improvements in windows.sh

--- src/dual-boot/windows.sh
+++ src/dual-boot/windows.sh
@@ -3,6 +3,12 @@
 # Grub will temporary change the default boot option
 # and restore to the original after reboot.
 
-# TODO: Detect Windows dynamically. It needs to filter submenus from list.
-sudo grub-reboot "Windows 10 (em /dev/sda2)"
+# TODO: Detect Windows dynamically.
+#       It needs to filter submenus from list.
+sudo grub-reboot "$(
+  grep -e '^menuentry' /boot/grub/grub.cfg |
+    nl -v 0 |
+    grep -e '--class windows' |
+    awk '{ print $1 }'
+)"
 sudo reboot
