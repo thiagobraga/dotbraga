cc5374f4dc132f353b5ebf325ebfdf87a85e21db
Thiago Braga <thibraga06@gmail.com>
2021-09-05 14:00:28 -0300

    feat(dual-boot): Add submenu entry filter for windows reboot

--- src/dual-boot/windows.sh
+++ src/dual-boot/windows.sh
@@ -2,11 +2,8 @@
 
 # Grub will temporary change the default boot option
 # and restore to the original after reboot.
-
-# TODO: Detect Windows dynamically.
-#       It needs to filter submenus from list.
 sudo grub-reboot "$(
-  grep -e '^menuentry' /boot/grub/grub.cfg |
+  grep -e '^menuentry' -e '^submenu' /boot/grub/grub.cfg |
     nl -v 0 |
     grep -e '--class windows' |
     awk '{ print $1 }'
