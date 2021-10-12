b0de2532447a3b33ea7b387422d0905af906472e
Thiago Braga <thibraga06@gmail.com>
2021-10-12 01:23:11 -0300

    feat(dualboot): Improve Windows detection

--- src/dual-boot/windows.sh
+++ src/dual-boot/windows.sh
@@ -2,5 +2,5 @@
 
 # Grub will temporary change the default boot option
 # and restore to the original after reboot.
-sudo grub-reboot "$(grep -e 'windows' /boot/grub/grub.cfg | grep -Eo '"(.*)"')"
+sudo grub-reboot "$(grep -i '^menuentry "Windows' /boot/grub/grub.cfg | head -n 1 | cut -d"'" -f2)"
 sudo reboot
