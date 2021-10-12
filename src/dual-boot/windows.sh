7f0a251eb1bf045a8d8d4f31f0b0d7ffe8d4ddab
Thiago Braga <thibraga06@gmail.com>
2021-10-11 23:39:35 -0300

    feat(dual-boot): Improve the search for Windows in grub list

--- src/dual-boot/windows.sh
+++ src/dual-boot/windows.sh
@@ -2,10 +2,5 @@
 
 # Grub will temporary change the default boot option
 # and restore to the original after reboot.
-sudo grub-reboot "$(
-  grep -e '^menuentry' -e '^submenu' /boot/grub/grub.cfg |
-    nl -v 0 |
-    grep -e '--class windows' |
-    awk '{ print $1 }'
-)"
+sudo grub-reboot "$(grep -e 'windows' /boot/grub/grub.cfg | grep -Eo '"(.*)"')"
 sudo reboot
