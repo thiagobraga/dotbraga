3664fc400935d9f9052f507bd386aefd97999b85
Thiago Braga <thibraga06@gmail.com>
2022-03-24 18:50:50 -0300

    feat: Improve Windows reboot command
    And create a win() function to be able to run manually.

--- src/dual-boot/windows.sh
+++ src/dual-boot/windows.sh
@@ -2,5 +2,13 @@
 
 # Grub will temporary change the default boot option
 # and restore to the original after reboot.
-sudo grub-reboot "$(grep -i '^menuentry "Windows' /boot/grub/grub.cfg | head -n 1 | cut -d"'" -f2)"
-sudo reboot
+win() {
+  sudo grub-reboot "$(
+    sudo grep -i '^menuentry.*Windows' /boot/grub/grub.cfg |
+      head -n 1 |
+      cut -d"'" -f4
+  )"
+  sudo reboot
+}
+
+win
