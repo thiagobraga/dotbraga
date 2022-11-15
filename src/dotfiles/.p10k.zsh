97361bd34faca3041051cbc91d2a18b9044ad633
Thiago Braga <thibraga06@gmail.com>
2022-11-15 20:20:10 -0300

    feat(p10k): Add context on left prompt

--- src/dotfiles/.p10k.zsh
+++ src/dotfiles/.p10k.zsh
@@ -32,6 +32,7 @@
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
+    context                 # user@hostname
     os_icon                 # os identifier
     dir                     # current directory
     # vcs                   # git status
@@ -86,7 +87,7 @@
     # gcloud                # google cloud cli account and project (https://cloud.google.com/)
     # google_app_cred       # google application credentials (https://cloud.google.com/docs/authentication/production)
     # toolbox               # toolbox name (https://github.com/containers/toolbox)
-    context                 # user@hostname
+    # context               # user@hostname
     # nordvpn               # nordvpn connection status, linux only (https://nordvpn.com/)
     # ranger                # ranger shell (https://github.com/ranger/ranger)
     # nnn                   # nnn shell (https://github.com/jarun/nnn)
