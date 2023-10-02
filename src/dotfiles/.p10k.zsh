f32c49f0ddfe966940cd1d8058df8f658445da37
Thiago Braga <thibraga06@gmail.com>
2023-10-02 12:16:54 -0300

    feat(zsh): Show powerlevel10k context root

--- src/dotfiles/.p10k.zsh
+++ src/dotfiles/.p10k.zsh
@@ -918,7 +918,7 @@
   typeset -g POWERLEVEL9K_CONTEXT_BACKGROUND=0
 
   # Context format when running with privileges: user@hostname.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%n@%m'
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%n'
   # Context format when in SSH without privileges: user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE='%n@%m'
   # Default context format (no privileges, no SSH): user@hostname.
